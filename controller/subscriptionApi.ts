836bac8f5e421d50b7cab7d4d621da94d684f8e4
gfjgabriel <gfjgabriel@gmail.com>
2021-10-19 22:08:38 -0300

    Code improvement

--- controller/subscriptionApi.ts
+++ controller/subscriptionApi.ts
@@ -4,9 +4,6 @@ import { api } from './index';
 
 export namespace ISubscriptionApi {
     export const Instance = {
-        isUserAllowedToLike: (): Promise<ApiResponse<boolean>> => {
-            return api.get<boolean>(`/subscriptions/allowed-to-like`);
-        },
         getCurrentUserSubscription: (): Promise<ApiResponse<Subscription>> => {
             return api.get(`/subscriptions`);
         }
